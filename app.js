2ed6b26241a9fc1910aa672a6d4d7136022874ea
Nghi <thanhnghi94@gmail.com>
2016-05-04 15:32:32 +0200

    Some minor change

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require("express");
+var express = require("express"),
+    path = require("path");
 
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render("index");
