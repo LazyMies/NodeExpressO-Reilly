d92c70c79c55a3bf38613c939eafd087eeabfe58
Nghi <thanhnghi94@gmail.com>
2016-05-04 15:15:40 +0200

    user app.render instead of plank page text/plain ,  add 500 error page

--- app.js
+++ app.js
@@ -5,29 +5,25 @@ var app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    res.type("text/plain");
-    res.send("Home Page");
+    res.render("index");
 });
 
 app.get("/about", function (req, res) {
-    res.type("text/plain");
-    res.send("About Page");
+    res.render("about");
 });
 
 
 // custom 404 page
 app.use(function (req, res) {
-    res.type("text/plain");
     res.status(404);
-    res.send("404 - not found");
+    res.render("404");
 });
 
 // custom 404 page
 app.use(function (err, req, res, next) {
     console.log(err.stack);
-    res.type("text/plain");
     res.status(500);
-    res.send("500 - Server Error");
+    res.render("500");
 });
 
 app.listen(app.get("port"), function () {
