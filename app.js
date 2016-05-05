8041d6f05c6a4fe504cfbc69e3f4cb4a43467585
Nghi <thanhnghi94@gmail.com>
2016-05-05 21:27:15 +0200

    app.js is still in the case when handlebars engine was not uninstalled. Just update it.

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 var express = require("express"),
-    path = require("path"),
-    handlebars = require("express-handlebars").create({defaultLayout: 'main'});
+    path = require("path");
+
+var directoryPath = path.join(__dirname, "public");
 
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
- app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render("index");
+    res.sendFile(path.join(directoryPath, "home.html"));
 });
 
 app.get("/about", function (req, res) {
-    res.render("about");
+    res.sendFile(path.join(directoryPath, "about.html"));
 });
 
 
 // custom 404 page
 app.use(function (req, res) {
     res.status(404);
-    res.render("404");
+    res.sendFile(path.join(directoryPath, "404.html"));
 });
 
 // custom 404 page
 app.use(function (err, req, res, next) {
     console.log(err.stack);
     res.status(500);
-    res.render("500");
+    res.sendFile(path.join(directoryPath, "500.html"));
 });
 
 app.listen(app.get("port"), function () {
