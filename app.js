2839bfb4d3bfa63ace09335c49f15046b38f1ea7
Nghi <thanhnghi94@gmail.com>
2016-05-05 20:22:19 +0200

    trying to send html with response object

--- app.js
+++ app.js
@@ -4,30 +4,27 @@ var express = require("express"),
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
- app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render("index");
+    res.sendfile("home.html");
 });
 
 app.get("/about", function (req, res) {
-    res.render("about");
+    res.sendfile("about.html");
 });
 
 
 // custom 404 page
 app.use(function (req, res) {
     res.status(404);
-    res.render("404");
+    res.sendfile("404.html");
 });
 
 // custom 404 page
 app.use(function (err, req, res, next) {
     console.log(err.stack);
     res.status(500);
-    res.render("500");
+    res.sendfile("500.html");
 });
 
 app.listen(app.get("port"), function () {
